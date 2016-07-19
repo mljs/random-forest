3c1330205e12573d971549b654718388c73ad434
jeffersonh44 <jeffersonh44@gmail.com>
2016-07-19 16:50:38 -0500

    Utils complete and tested.

---
+++ src/Utils.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var Random = require("random-js");
+var Matrix = require("ml-matrix");
+
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+}
+
+function examplesBaggingWithReplacement(X, y, seed) {
+    var engine = Random.engines.mt19937();
+    var distribution = Random.integer(0, X.rows - 1);
+    if(seed === undefined) {
+        engine = engine.autoSeed();
+    } else if(isInt(seed)) {
+        engine = engine.seed(seed);
+    } else {
+        throw new RangeError("Expected seed must be undefined or integer not " + seed);
+    }
+
+    var Xr = new Array(X.rows);
+    var yr = new Array(X.rows);
+
+    for(var i = 0; i < X.rows; ++i) {
+        var index = distribution(engine);
+        Xr[i] = X[index];
+        yr[i] = y[index];
+    }
+
+    return {
+        X: new Matrix(Xr),
+        y: yr
+    }
+}
+
+function featureBagging(X, n, replacement, seed) {
+    if(X.columns < n) {
+        throw new RangeError("N should be lesser or equal to the number of columns of X");
+    }
+
+    var distribution = Random.integer(0, X.columns - 1);
+    var engine = Random.engines.mt19937();
+    if(seed === undefined) {
+        engine = engine.autoSeed();
+    } else if(isInt(seed)) {
+        engine = engine.seed(seed);
+    } else {
+        throw new RangeError("Expected seed must be undefined or integer not " + seed);
+    }
+
+    var toRet = new Matrix(X.rows, n);
+
+    if(replacement) {
+        var usedIndex = new Array(n);
+        for(var i = 0; i < n; ++i) {
+            var index = distribution(engine);
+            usedIndex[i] = index;
+            toRet.setColumn(i, X.getColumn(index));
+        }
+    } else {
+        usedIndex = new Set();
+        index = distribution(engine);
+        for(i = 0; i < n; ++i) {
+            while(usedIndex.has(index)) {
+                index = distribution(engine);
+            }
+            toRet.setColumn(i, X.getColumn(index));
+        }
+        usedIndex = Array.from(usedIndex);
+    }
+
+    return {
+        X: toRet,
+        usedIndex: usedIndex
+    };
+}
+
+function retrieveFeatures(X, indexes) {
+    var toRet = new Matrix(X.rows, indexes.length);
+    for(var i = 0; i < indexes.length; ++i) {
+        toRet.setColumn(i, X.getColumn(indexes[i]));
+    }
+
+    return toRet;
+}
+
+module.exports = {
+    examplesBaggingWithReplacement: examplesBaggingWithReplacement,
+    featureBagging: featureBagging,
+    retrieveFeatures: retrieveFeatures
+};
--- test/test.js
+++ test/test.js
@@ -1,1 +1,34 @@
 'use strict';
+
+var Utils = require("../src/Utils");
+var Matrix = require("ml-matrix");
+
+describe('Utils', function () {
+    it('Retrieve features', function () {
+        var data = new Matrix([[1, 2, 3, 4, 5],
+                    [1, 2, 3, 4, 5],
+                    [1, 2, 3, 4, 5]]);
+        var indexes = [0, 4];
+
+        var newData = Utils.retrieveFeatures(data, indexes);
+        for(var i = 0; i < newData.column; ++i) {
+            newData[i].should.be.deepEqual([1, 5]);
+        }
+    });
+
+    var rows = 40, cols = 25;
+    var testX = Matrix.rand(rows, cols);
+    var testY = new Array(rows).fill(1);
+
+    it('Examples bagging', function () {
+        var data = Utils.examplesBaggingWithReplacement(testX, testY);
+
+        data.X.should.be.instanceOf(Matrix);
+    });
+
+    it('Feature bagging', function () {
+        var data = Utils.featureBagging(testX, cols - 5, true, 7);
+        new Set(data.usedIndex).size.should.be.lessThan(data.usedIndex.length);
+        data.X.columns.should.be.equal(20);
+    });
+});
