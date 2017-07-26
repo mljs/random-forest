3d57ce7db23bde55c2a4879941e1c54cc551e497
Jefferson Hernández <jeffersonh44@gmail.com>
2017-07-26 10:13:05 -0500

    remove default from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {default as RandomForestClassifier} from './RandomForestClassifier';
 import {default as RandomForestRegression} from './RandomForestRegression';
 
-export default {
+export {
     RandomForestClassifier,
     RandomForestRegression
 };
