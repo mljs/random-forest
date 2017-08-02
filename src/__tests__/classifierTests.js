6702ecf19d122afc6e23f7d013f992814537ae3a
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-08-02 10:35:38 +0200

    test: fix test structure

--- src/__tests__/classifierTests.js
+++ src/__tests__/classifierTests.js
@@ -1,25 +1,23 @@
 import IrisDataset from 'ml-dataset-iris';
 import {RandomForestClassifier as RFClassifier} from '..';
 
-describe('Random Forest Classifier', function () {
+var trainingSet = IrisDataset.getNumbers();
+var predictions = IrisDataset.getClasses().map(elem => IrisDataset.getDistinctClasses().indexOf(elem));
 
-    var trainingSet = IrisDataset.getNumbers();
-    var predictions = IrisDataset.getClasses().map(elem => IrisDataset.getDistinctClasses().indexOf(elem));
+var options = {
+    seed: 3,
+    maxFeatures: 0.8,
+    replacement: true,
+    nEstimators: 25,
+    treeOptions: undefined // default options for the decision tree
+};
 
-    var options = {
-        seed: 3,
-        maxFeatures: 0.8,
-        replacement: true,
-        nEstimators: 25,
-        treeOptions: undefined // default options for the decision tree
-    };
+var classifier = new RFClassifier(options);
+classifier.train(trainingSet, predictions);
+var result = classifier.predict(trainingSet);
 
-    var classifier = new RFClassifier(options);
-    classifier.train(trainingSet, predictions);
-    var result = classifier.predict(trainingSet);
-
-
-    test('Random Forest Classifier with iris dataset', function () {
+describe('Random Forest Classifier', () => {
+    test('Random Forest Classifier with iris dataset', () => {
         var correct = 0;
         for (var i = 0; i < result.length; ++i) {
             if (result[i] === predictions[i]) correct++;
@@ -29,7 +27,7 @@ describe('Random Forest Classifier', function () {
         expect(score).toBeGreaterThanOrEqual(0.7); // above or equal
     });
 
-    test('Export and import for random forest classifier', function () {
+    test('Export and import for random forest classifier', () => {
         var model = JSON.parse(JSON.stringify(classifier));
 
         var newClassifier = RFClassifier.load(model);
