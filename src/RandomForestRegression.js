dde31eae3e24a8050c3bb61b84fb14fe078b674d
micha-lmxt <michael@pop-os.localdomain>
2020-10-12 13:54:27 +0200

    revert previous branch

--- src/RandomForestRegression.js
+++ src/RandomForestRegression.js
@@ -11,11 +11,11 @@ const selectionMethods = {
 const defaultOptions = {
   maxFeatures: 1.0,
   replacement: false,
-  nEstimators: 50,
+  nEstimators: 10,
   treeOptions: {},
   selectionMethod: 'mean',
   seed: 42,
-  useSampleBagging: true,
+  useSampleBagging: false,
 };
 
 /**
@@ -32,10 +32,10 @@ export class RandomForestRegression extends RandomForestBase {
    *        * if is a float between (0, 1), it takes the percentage of features.
    * @param {boolean} [options.replacement=true] - use replacement over the sample features.
    * @param {number} [options.seed=42] - seed for feature and samples selection, must be a 32-bit integer.
-   * @param {number} [options.nEstimators=50] - number of estimator to use.
+   * @param {number} [options.nEstimators=10] - number of estimator to use.
    * @param {object} [options.treeOptions={}] - options for the tree classifier, see [ml-cart]{@link https://mljs.github.io/decision-tree-cart/}
    * @param {string} [options.selectionMethod="mean"] - the way to calculate the prediction from estimators, "mean" and "median" are supported.
-   * @param {boolean} [options.useSampleBagging=true] - use bagging over training samples.
+   * @param {boolean} [options.useSampleBagging=false] - use bagging over training samples.
    * @param {object} model - for load purposes.
    */
   constructor(options, model) {
