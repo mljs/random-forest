11a703b0227d853291b05568b69c39cc64ba44b7
Michael Lucht <44426265+micha-lmxt@users.noreply.github.com>
2020-10-15 09:40:48 +0200

    change defaults (#19)
    Co-authored-by: micha-lmxt <michael@pop-os.localdomain>

--- src/RandomForestClassifier.js
+++ src/RandomForestClassifier.js
@@ -3,9 +3,9 @@ import { RandomForestBase } from './RandomForestBase';
 const defaultOptions = {
   maxFeatures: 1.0,
   replacement: true,
-  nEstimators: 10,
+  nEstimators: 50,
   seed: 42,
-  useSampleBagging: false,
+  useSampleBagging: true,
 };
 
 /**
@@ -22,9 +22,9 @@ export class RandomForestClassifier extends RandomForestBase {
    *        * if is a float between (0, 1), it takes the percentage of features.
    * @param {boolean} [options.replacement=true] - use replacement over the sample features.
    * @param {number} [options.seed=42] - seed for feature and samples selection, must be a 32-bit integer.
-   * @param {number} [options.nEstimators=10] - number of estimator to use.
+   * @param {number} [options.nEstimators=50] - number of estimator to use.
    * @param {object} [options.treeOptions={}] - options for the tree classifier, see [ml-cart]{@link https://mljs.github.io/decision-tree-cart/}
-   * @param {boolean} [options.useSampleBagging=false] - use bagging over training samples.
+   * @param {boolean} [options.useSampleBagging=true] - use bagging over training samples.
    * @param {object} model - for load purposes.
    */
   constructor(options, model) {
